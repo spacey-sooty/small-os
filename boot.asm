e18d807cc47b62b979763f8feb889a60c0dc2025
Isaac Turner <spacey_sooty@outlook.com>
2023-11-14 22:56:12 +0800

    use some control flow to print easier

--- boot.asm
+++ boot.asm
@@ -5,7 +5,7 @@ mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get ov
 mov sp, bp ; if the stack is empty then sp points to bp
 
 mov ah, 0x0e ; tty mode
-mov al, [the_data]
+mov al, "H"
 int 0x10
 
 push 'o'
@@ -32,21 +32,35 @@ mov al, ','
 int 0x10
 mov al, ''
 int 0x10
-mov al, 'w'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10
-mov al, '!'
-int 0x10
+mov bx, the_data
+call print
+
+print:
+    pusha
+
+; keep this in mind:
+; while (string[i] != 0) { print string[i]; i++ }
+
+; the comparison for string end (null byte)
+
+    mov al, [bx] ; 'bx' is the base address for the string
+    cmp al, 0 
+    je done
+
+    ; the part where we print with the BIOS help
+    mov ah, 0x0e
+    int 0x10 ; 'al' already contains the char
+
+    ; increment pointer and do next loop
+    add bx, 1
+    jmp print
+
+done:
+    popa
+    ret
 
 the_data:
-    db "H"
+    db 'world!', 0
 
 jmp $ ; jump to current address = infinite loop
 
